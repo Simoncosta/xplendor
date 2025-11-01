6c5dda3d1df5bbbbe9c542bdd3b81f16178d3f48
Simon Costa <saimom_costa@hotmail.com>
2025-11-01 13:22:19 +0000

    include a folder web

---
+++ web/vite.config.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler", // or "modern"
+        silenceDeprecations: ["import", "global-builtin"],
+      },
+    },
+  },
+});
